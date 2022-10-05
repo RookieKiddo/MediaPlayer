8e90c37065854ac1b606bac82277c682b17259cf
RookieKiddo <aheeduddin50@gmail.com>
2022-10-05 12:43:56 +0500

    first commit

---
+++ MediaPlayerOOP2B/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace MediaPlayerOOP2B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAction = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAction
+            // 
+            this.btnAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAction.Image = ((System.Drawing.Image)(resources.GetObject("btnAction.Image")));
+            this.btnAction.Location = new System.Drawing.Point(16, 389);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(94, 61);
+            this.btnAction.TabIndex = 0;
+            this.btnAction.Text = "Play";
+            this.btnAction.UseVisualStyleBackColor = true;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.Location = new System.Drawing.Point(116, 389);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(91, 61);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(288, 389);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 29);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(12, 361);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.ReadOnly = true;
+            this.txtSearch.Size = new System.Drawing.Size(432, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(369, 389);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 29);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 343);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(458, 459);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnAction);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MediaPlayerOOP2B/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace MediaPlayerOOP2B
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string A, string B, int C, int D);
+        string F;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile("Media//bg1.jpg");
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAction_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text.Length == 0)
+            {
+                btnBrowse_Click(null, null);
+            }
+            F = "open "+txtSearch.Text+" type MPEGVideo alias ABC";
+            mciSendString(F, null, 0, 0);
+            if (btnAction.Text.Equals("Play"))
+            {
+                mciSendString("play ABC", null, 0, 0);
+                btnAction.Text = "Pause";
+                btnAction.Image = Image.FromFile("Media//Pause.png");
+                pictureBox1.Image = Image.FromFile("Media//waves.gif");
+            }
+            else
+            {
+                mciSendString("pause ABC", null, 0, 0);
+                btnAction.Text = "Play";
+                btnAction.Image = Image.FromFile("Media//Play.png");
+                pictureBox1.Image = Image.FromFile("Media//bg1.jpg");
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            mciSendString("close ABC", null, 0, 0);
+            btnAction.Text = "Play";
+            btnAction.Image = Image.FromFile("Media//Play.png");
+            pictureBox1.Image = Image.FromFile("Media//bg1.jpg");
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OFD = new OpenFileDialog();
+            OFD.InitialDirectory = "F:/AudioNaats/";
+            DialogResult DR = OFD.ShowDialog();
+            if (DR == DialogResult.OK)
+            {
+                txtSearch.Text = OFD.FileName;
+            }
+        }
+    }
+}
